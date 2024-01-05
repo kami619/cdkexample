a373c2668f259fe5ed598b8e2d8a58e465381089
Kamesh Akella <kamesh.asp@gmail.com>
2024-01-05 14:51:56 +0530

    aurora rds cluster
    Signed-off-by: Kamesh Akella <kamesh.asp@gmail.com>

--- src/main/java/com/myorg/CdkWorkshopApp.java
+++ src/main/java/com/myorg/CdkWorkshopApp.java
@@ -2,7 +2,7 @@ package com.myorg;
 
 import software.amazon.awscdk.App;
 
-public final class CdkWorkshopApp {
+public final class     CdkWorkshopApp {
     public static void main(final String[] args) {
         App app = new App();
 
--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,12 +1,12 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.ec2.SubnetSelection;
+import software.amazon.awscdk.services.ec2.SubnetType;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 import software.constructs.Construct;
-import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
 
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
@@ -16,14 +16,22 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
+        Vpc vpc = Vpc.Builder.create(this, "MyVpc")
+                .maxAzs(2) // Adjust based on your needs
                 .build();
 
-        final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
-
-        topic.addSubscription(new SqsSubscription(queue));
+        DatabaseCluster auroraCluster = DatabaseCluster.Builder.create(this, "MyAuroraCluster")
+                .engine(DatabaseClusterEngine.auroraPostgres(
+                        AuroraPostgresClusterEngineProps.builder()
+                                .version(AuroraPostgresEngineVersion.VER_15_5) // Specify your version
+                                .build()))
+                .instanceProps(InstanceProps.builder()
+                        .vpc(vpc)
+                        .vpcSubnets(SubnetSelection.builder()
+                                .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
+                                .build())
+                        .build())
+                .instances(2) // For multi-AZ
+                .build();
     }
 }
